1db8e8de71a0a19aa37ee52562d37ea35c777159
Yasmina Baraza Albertos <dj_yasminafter@hotmail.com>
2025-12-09 18:47:58 +0100

    versio 1.0

---
+++ Desktop/IOC/M08/EAC4/ModaGlobal/src/main/java/cat/xtec/ioc/modaglobal/ModaGlobal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cat.xtec.ioc.modaglobal;
+
+/**
+ *
+ * @author dj_ya
+ */
+public class ModaGlobal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
